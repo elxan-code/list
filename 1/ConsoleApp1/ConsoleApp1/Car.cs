1647dec632d1e6b52d873040410418af21b30577
unknown <you@example.com>
2021-11-12 01:05:27 +0400

    lists

---
+++ 1/ConsoleApp1/ConsoleApp1/Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Car
+    {
+        public string brand;
+        public string model;
+        public string doorcount;
+        public Car(string brand, string model, string doorcount)
+        {
+            this.brand = brand;
+            this.model = model;
+            this.doorcount = doorcount;
+
+
+        }
+
+        public string FullInfo()
+        {
+            return $"Brand is: {brand}, Model is : {model}, Doorcount is : {doorcount}";
+
+
+        }
+    }
+}
---
+++ 1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region task1
+            //List<int> salam = new List<int>(new int[] { 1, 2, 3, 4 });
+            //foreach (var item in salam)
+            //{
+            //    int c = item * 3;
+            //    Console.Write(c.ToString()+ " ");
+            //}
+            #endregion
+
+            #region task2
+            //List<string> salam = new List<string>(new string[] { "A" ,"B", "C","D" });
+            //foreach (var item in salam)
+            //{
+
+            //    Console.WriteLine("#"+item.ToString()+"#");
+            //}
+            #endregion
+
+
+
+            //List<byte> salam = new List<byte>();
+
+
+            //salam.Add(5);
+            //salam.Add(20);
+            //salam.Add(45);
+            //foreach (var item in salam)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //salam.RemoveAll();
+
+            //List<Car> salam = new List<Car>();
+            //{
+            //    Car c1 = new Car("Bmw", "m5", "5");
+            //    Car c2 = new Car("Bmw", "x5", "5");
+            //    Car c3 = new Car("Lada", "2107", "5");
+            //    Car c4 = new Car("Lada", "Priora", "5");
+            //    Car c5 = new Car("Nissan", "GTR", "3");
+            //}
+            //foreach (var item in salam)
+            //{
+            //    Console.WriteLine(item.FullInfo());
+            //}
+            
+        }
+    }
+}
